ca36247615a55ca8efb3f092406f7ce62ee9a006
antonpash <antonpash@gmail.com>
2017-09-30 15:47:46 +0300

    woocommerce templates updates

--- wp-content/themes/cre8or/woocommerce/cart/mini-cart.php
+++ wp-content/themes/cre8or/woocommerce/cart/mini-cart.php
@@ -73,7 +73,8 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 	<div class="woocommerce-mini-cart__buttons buttons">
 		<p class="woocommerce-mini-cart__total total"><strong><?php _e( 'Subtotal', 'woocommerce' ); ?>:</strong> <?php echo WC()->cart->get_cart_subtotal(); ?></p>
 
-		<?php do_action( 'woocommerce_widget_shopping_cart_buttons' ); ?>
+		<?php echo '<a href="' . esc_url( wc_get_cart_url() ) . '" class="button-xs align-center wc-forward">' . esc_html__( 'View cart', 'woocommerce' ) . '</a>'; ?>
+		<?php echo '<a href="' . esc_url( wc_get_checkout_url() ) . '" class="button-sm button-outlined align-center checkout wc-forward">' . esc_html__( 'Checkout', 'woocommerce' ) . '</a>'; ?>
 	</div>
 
 <?php else : ?>
