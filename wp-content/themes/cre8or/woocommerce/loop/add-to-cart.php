cab7e64e3b8bacbdaca8efbb39975c350c468cc0
antonpash <antonpash@gmail.com>
2017-10-01 19:31:26 +0300

    woocommerce templates updates

--- wp-content/themes/cre8or/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/cre8or/woocommerce/loop/add-to-cart.php
@@ -2,24 +2,33 @@
 /**
  * Loop Add to Cart
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.1.0
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+    exit;
 }
 
 global $product;
-//printf( '<a href="?add-to-cart=%s" rel="nofollow" data-product_id="%s" data-product_sku="" data-quantity="1" class="">Добавить в корзину</a>', $product_id, $product_id );
+
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-	sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button-sm button-outlined text-black align-center uppercase %s product_type_%s"><i data-icon="ios-cart" data-icon-color="#000" data-icon-size="28"></i> '.__( 'Add to cart', 'woocommerce' ).'</a>',
-		esc_url( $product->add_to_cart_url() ),
-		esc_attr( $product->id ),
-		esc_attr( $product->get_sku() ),
-		esc_attr( isset( $quantity ) ? $quantity : 1 ),
-		$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
-		esc_attr( $product->product_type )
-	),
-$product );
+    sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
+        esc_url( $product->add_to_cart_url() ),
+        esc_attr( isset( $quantity ) ? $quantity : 1 ),
+        esc_attr( $product->get_id() ),
+        esc_attr( $product->get_sku() ),
+        esc_attr( isset( $class ) ? $class : 'button' ),
+        esc_html( $product->add_to_cart_text() )
+    ),
+    $product );
