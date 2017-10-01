c006d81fd084c6db4814a2ff7bcb73f7019a6d48
antonpash <antonpash@gmail.com>
2017-10-01 19:47:09 +0300

    woocommerce templates updates

--- wp-content/themes/cre8or/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/cre8or/woocommerce/single-product/add-to-cart/simple.php
@@ -2,62 +2,69 @@
 /**
  * Simple product add to cart
  *
- * @author        WooThemes
- * @package    WooCommerce/Templates
- * @version     2.1.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/simple.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.0.0
  */
-
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
 
 global $product;
 
-if (!$product->is_purchasable()) {
+if ( ! $product->is_purchasable() ) {
     return;
 }
 
-?>
+echo wc_get_stock_html( $product );
 
-<li>
-    <?php
-    // Availability
-    $availability = $product->get_availability();
-    $availability_html = empty($availability['availability']) ? '' : '<p class="stock ' . esc_attr($availability['class']) . '">' . esc_html($availability['availability']) . '</p>';
+if ( $product->is_in_stock() ) : ?>
 
-    echo apply_filters('woocommerce_stock_html', $availability_html, $availability['availability'], $product);
-    ?>
+    <?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-    <?php if ($product->is_in_stock()) : ?>
+    <form class="cart" method="post" enctype='multipart/form-data'>
+        <?php
+        /**
+         * @since 2.1.0.
+         */
+        do_action( 'woocommerce_before_add_to_cart_button' );
 
-        <?php do_action('woocommerce_before_add_to_cart_form'); ?>
+        /**
+         * @since 3.0.0.
+         */
+        do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-        <form class="cart" method="post" enctype='multipart/form-data'>
-            <?php do_action('woocommerce_before_add_to_cart_button'); ?>
+        woocommerce_quantity_input( array(
+            'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+            'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+            'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
+        ) );
 
-            <?php
-            if (!$product->is_sold_individually()) {
-                woocommerce_quantity_input(array(
-                    'min_value' => apply_filters('woocommerce_quantity_input_min', 1, $product),
-                    'max_value' => apply_filters('woocommerce_quantity_input_max', $product->backorders_allowed() ? '' : $product->get_stock_quantity(), $product),
-                    'input_value' => (isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : 1)
-                ));
-            }
-            ?>
+        /**
+         * @since 3.0.0.
+         */
+        do_action( 'woocommerce_after_add_to_cart_quantity' );
+        ?>
 
-            <input type="hidden" name="add-to-cart" value="<?php echo esc_attr($product->id); ?>"/>
+        <button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
-            <button type="submit"
-                    class="single_add_to_cart_button button-sm button-outlined text-black align-left uppercase"
-                    style="margin:0;">
-                <?php echo esc_html($product->single_add_to_cart_text()); ?>
-                <i data-icon="ios-cart" data-icon-color="#000" data-icon-size="28"></i>
-            </button>
+        <?php
+        /**
+         * @since 2.1.0.
+         */
+        do_action( 'woocommerce_after_add_to_cart_button' );
+        ?>
+    </form>
 
-            <?php do_action('woocommerce_after_add_to_cart_button'); ?>
-        </form>
+    <?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
 
-        <?php do_action('woocommerce_after_add_to_cart_form'); ?>
-
-    <?php endif; ?>
-</li>
+<?php endif; ?>
