54331552ca509e489da02f39d94baca80a9e88e2
Anton Pashkovskyi <antonpash@gmail.com>
2017-10-15 14:16:21 +0300

    general updates

--- wp-content/plugins/delivery-in-nova-pochta/delivery-in-nova-pochta.php
+++ wp-content/plugins/delivery-in-nova-pochta/delivery-in-nova-pochta.php
@@ -3,9 +3,11 @@
 Plugin Name: Saphali WooCommerce Nova Pocta
 Plugin URI: http://saphali.com/saphali-woocommerce-plugin-wordpress
 Description: Nova Pocta - Позволяет сразу выбрать и оформить доставку в РЕГИОНЫ через транспортные компании при оформлении заказа. Подробнее на сайте <a href="http://saphali.com/saphali-woocommerce-plugin-wordpress">Saphali Woocommerce</a>
-Version: 1.0.2
+Version: 1.0.4
 Author: Saphali
 Author URI: http://saphali.com/
+Text Domain: wc-saphali-nova-pocta
+Domain Path: /languages
 
 */
 
@@ -28,12 +30,12 @@ if ( version_compare( WOOCOMMERCE_VERSION, '2.0', '<' ) ) {} else {
 class WC_New_Pochta extends WC_Free_Shipping {
 	function __construct() {
         $this->id 			= 'nova_pochta';
-        $this->method_title = __('Новая почта', 'woocommerce');
+        $this->method_title = __('Новая почта', 'wc-saphali-nova-pocta');
 		$this->init();
 		add_filter('woocommerce_cart_shipping_method_full_label', array($this, 'woocommerce_cart_shipping_method_full_label') );
-    }
+    } 
 	function woocommerce_cart_shipping_method_full_label($full_label) {
-		$_full_label = str_replace(' (' . __( 'Free', 'woocommerce' ) . ')', '', $full_label);
+		$_full_label = str_replace(' (' . __( 'Free', 'wc-saphali-nova-pocta' ) . ')', '', $full_label);
 		if( $this->title ==  $_full_label) {
 			$full_label = $_full_label;
 		}
@@ -50,7 +52,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 			'enabled' => array(
 							'title' 		=> __( 'Enable/Disable', 'woocommerce' ),
 							'type' 			=> 'checkbox',
-							'label' 		=> __( 'Активировать Новую почту', 'woocommerce' ),
+							'label' 		=> __( 'Активировать Новую почту', 'wc-saphali-nova-pocta' ),
 							'default' 		=> 'yes'
 						),
 			'title' => array(
@@ -72,7 +74,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 								'step'	=> 'any',
 								'min'	=> '0'
 							),
-							'description' 	=> 'Добавочная стоимость при выборе этого метода доставки.',
+							'description' 	=> __('Добавочная стоимость при выборе этого метода доставки.', 'wc-saphali-nova-pocta'),
 							'default'		=> '',
 							'desc_tip'      => true,
 							'placeholder'	=> '0.00'
@@ -98,14 +100,14 @@ class WC_New_Pochta extends WC_Free_Shipping {
 			'enabled' => array(
 							'title' 		=> __( 'Enable/Disable', 'woocommerce' ),
 							'type' 			=> 'checkbox',
-							'label' 		=> __( 'Активировать Новую почту', 'woocommerce' ),
+							'label' 		=> __( 'Активировать Новую почту', 'wc-saphali-nova-pocta' ),
 							'default' 		=> 'yes'
 						),
 			'title' => array(
 							'title' 		=> __( 'Method Title', 'woocommerce' ),
 							'type' 			=> 'text',
 							'description' 	=> __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-							'default'		=> __( 'Новая почта', 'woocommerce' ),
+							'default'		=> __( 'Новая почта', 'wc-saphali-nova-pocta' ),
 							'desc_tip'      => true,
 						),
 
@@ -128,12 +130,12 @@ class WC_New_Pochta extends WC_Free_Shipping {
 							'options'		=> $woocommerce->countries->countries
 						),
 			'requires' => array(
-							'title' 		=> __( 'Новая почта (опции задействия)', 'woocommerce' ),
+							'title' 		=> __( 'Новая почта (опции задействия)', 'wc-saphali-nova-pocta' ),
 							'type' 			=> 'select',
 							'default' 		=> $default_requires,
 							'options'		=> array(
 								'' 				=> __( 'N/A', 'woocommerce' ),
-								'coupon'		=> __( 'Купон на бесплатную доставку. Активация новой почты', 'woocommerce' ),
+								'coupon'		=> __( 'Купон на бесплатную доставку. Активация новой почты', 'wc-saphali-nova-pocta' ),
 								'min_amount' 	=> __( 'A minimum order amount (defined below)', 'woocommerce' ),
 								'either' 		=> __( 'A minimum order amount OR a coupon', 'woocommerce' ),
 								'both' 			=> __( 'A minimum order amount AND a coupon', 'woocommerce' ),
@@ -146,7 +148,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 								'step'	=> 'any',
 								'min'	=> '0'
 							),
-							'description' 	=> __( 'Покупателям необходимо потратить именно столько, чтобы активировать данный метод доставки. Оставте пустым, чтобы опция была неактивной.', 'woocommerce' ),
+							'description' 	=> __( 'Покупателям необходимо потратить именно столько, чтобы активировать данный метод доставки. Оставте пустым, чтобы опция была неактивной.', 'wc-saphali-nova-pocta' ),
 							'default' 		=> '0',
 							'desc_tip'      => true,
 							'placeholder'	=> '0.00'
@@ -158,7 +160,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 								'step'	=> 'any',
 								'min'	=> '0'
 							),
-							'description' 	=> 'Добавочная стоимость при выборе этого метода доставки.',
+							'description' 	=> __('Добавочная стоимость при выборе этого метода доставки.', "wc-saphali-nova-pocta"),
 							'default'		=> '',
 							'desc_tip'      => true,
 							'placeholder'	=> '0.00'
@@ -192,19 +194,6 @@ class WC_New_Pochta extends WC_Free_Shipping {
 		if ( $this->is_availables( ) ) {
 			add_action( 'woocommerce_review_order_after_shipping',  array( $this, 'review_order_pickup_location' ) );
 			add_action( 'woocommerce_checkout_update_order_meta',   array( $this, 'checkout_update_order_meta' ), 10, 2 );
-			add_action( 'woocommerce_thankyou',                     array( $this, 'order_pickup_location' ), 20 );
-			add_action( 'woocommerce_view_order',                   array( $this, 'order_pickup_location' ), 20 );
-			add_action( 'woocommerce_after_template_part',          array( $this, 'email_pickup_location' ), 10, 3 );
-					
-					add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_order_status_pending_to_completed_notification',  array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_order_status_pending_to_on-hold_notification',    array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_order_status_failed_to_processing_notification',  array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_order_status_failed_to_completed_notification',   array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_order_status_completed_notification',             array( $this, 'store_order_id' ), 1 );
-					add_action( 'woocommerce_new_customer_note_notification',                  array( $this, 'store_order_id' ), 1 );
-				
-				
 		}
 		add_filter( 'woocommerce_shipping_methods', array( $this, 'add_nova_pocha' ) );
 
@@ -246,7 +235,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 		}
 			if(!empty($pickup_location_array)) {
 				echo '<div>';
-				echo '<h4>'.__( 'Отделение Новой Почты:','delivery_in_regions' ).'</h4>';
+				echo '<h3>'.__( 'Адрес или № склада Новой Почты:', 'wc-saphali-nova-pocta' ).'</h3>';
 				
 				echo '<div> '.$pickup_location_array .'</div>';
 				echo '</div>';
@@ -259,67 +248,20 @@ class WC_New_Pochta extends WC_Free_Shipping {
 		if ( $posted['shipping_method'] == $this->id ) {
 
 			if ( isset($_POST['nova_pochta']) ) {
-				
-				if(!add_post_meta( $order_id, '_nova_pochta', esc_attr($_POST["nova_pochta"]) )) update_post_meta( $order_id, '_nova_pochta', esc_attr($_POST["nova_pochta"]) );
+				update_post_meta( $order_id, '_nova_pochta', esc_attr($_POST["nova_pochta"]) );
+				if( is_user_logged_in() ) {
+					$ob = wp_get_current_user(); 
+					update_user_meta( $ob->data->ID, 'billing_nova_post', $_POST["nova_pochta"] );
+				}
 			}
 		}
 	}
-
-	function order_pickup_location($order_id) {
-		$order = new WC_Order( $order_id );
-
-		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
-			$is_true = ( $order->get_shipping_method() == $this->title  ) ;
-		} else {
-			$is_true = ($order->shipping_method == $this->id  ); 
-		}
-		if ( $is_true ) {
-			if ( version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
-				$pickup_location_array = maybe_unserialize( $order->order_custom_fields['_nova_pochta'][0] );
-			} else {
-				$pickup_location_array =  get_post_meta($order->id, '_nova_pochta', true);			
-			}
-
-			if(!empty($pickup_location_array)) {
-				echo 'Отделение Новой Почты: '.$pickup_location_array;
-			}
-			
-		}
-		echo '</td></tr></table>';
+	public function is_available(  $package = array()  ) {
+		return $this->is_availables(  $package = array()  );
 	}
-	
-	public function store_order_id( $arg ) {
-		if ( is_int( $arg ) ) $this->email_order_id = $arg;
-		elseif ( is_array( $arg ) && array_key_exists( 'order_id', $arg ) ) $this->email_order_id = $arg['order_id'];
-	}
-	public function email_pickup_location( $template_name, $template_path, $located ) {
-				
-		if ( $template_name == 'emails/email-addresses.php' && $this->email_order_id ) {
-
-			$order = new WC_Order( $this->email_order_id );
-
-		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
-			$is_true = ( $order->get_shipping_method() == $this->title  ) ;
-		} else {
-			$is_true = ($order->shipping_method == $this->id  ); 
-		}
-		if ( $is_true ) {
-			if ( version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
-				$pickup_location_array = maybe_unserialize( $order->order_custom_fields['_nova_pochta'][0] );
-			} else {
-				$pickup_location_array =  get_post_meta($order->id, '_nova_pochta', true);			
-			}
-
-			if(!empty($pickup_location_array)) {
-				echo 'Отделение Новой Почты: '.$pickup_location_array.'</span></td></tr></table>';
-			}
-			}
-		}
-	}
-	
-	public function is_availables() {
-				
-		if ( $this->enabled == "no"  ) return false;
+	public function is_availables(  $package = array()  ) {
+		
+		if ( $this->enabled == "no"  ) {return false;}
 		
 	if(version_compare( WOOCOMMERCE_VERSION, '2.0', '<' )) {
 		global $woocommerce;
@@ -430,7 +372,7 @@ class WC_New_Pochta extends WC_Free_Shipping {
 		
 	}
 		if(!$is_available) $this->enabled = "no";
-		return apply_filters( 'woocommerce_shipping_' . $this->id . '_is_available', $is_available );
+		return apply_filters( 'woocommerce_shipping_' . $this->id . '_is_available', $is_available, $package );
 	}
 	
 	public function review_order_pickup_location() {
@@ -439,13 +381,28 @@ class WC_New_Pochta extends WC_Free_Shipping {
 		else $chosen_shipping_method = $woocommerce->session->chosen_shipping_method;
 		if( isset($_POST['shipping_method']) && is_array($_POST['shipping_method']) && isset($_POST['shipping_method'][0]) ) $posted['shipping_method'] = $_POST['shipping_method'][0]; 
 		if(isset($posted['shipping_method']) && is_null($chosen_shipping_method) )$chosen_shipping_method = $posted['shipping_method'];
-
+		if( ! $chosen_shipping_method ) {
+			$post_data = explode( '&', $_POST['post_data'] );
+			foreach ( $post_data as $data ) {
+				if ( strpos( $data, '=' ) !== false ) {
+					list( $name, $value ) = explode( '=', $data );
+					if( $name == 'shipping_method' || $name == 'shipping_method%5B0%5D' ) {
+						$chosen_shipping_method = $value;
+						break;
+					}
+				}
+			}	
+		}
 		if ( $chosen_shipping_method == $this->id ) {
 			echo '<tr class="free_shipping">';
-			echo '	<th align="right"><div style="width: auto; text-align:left;">' . __( 'Отделение', 'delivery_in_regions'  )  . '</div></th>';
-			echo '	<td style="line-height:0;">';
+			echo '	<th align="right"><div style="width: auto; float: right;">' . __( 'Адрес или № склада Новой Почты', 'wc-saphali-nova-pocta'  ) . ' <span style="color: red" title="обов\'язково">*</span>'  . '</div></th>';
+			echo '	<td>';
 			do_action( 'woocommerce_review_order_before_'.$this->id, array('nova_pochta') );
-			echo '<input type="text" id="nova_pochta" style="line-height:1; font-weight:normal; width:100%;" name="nova_pochta" value="" />&nbsp;';
+			if( is_user_logged_in() ) {
+				$ob = wp_get_current_user(); $zn = get_user_meta( $ob->data->ID, 'billing_nova_post', true);
+			}
+			else $zn = '';
+			echo '<input type="text" id="nova_pochta" name="nova_pochta" value="' . $zn . '" />&nbsp;';
 			echo '</td>';
 			
 			echo '</tr>';
@@ -463,7 +420,164 @@ new WC_New_Pochta();
 
 }
 
-add_action( 'woocommerce_shipping_init', 'woocommerce_nova_pochta_init');
+
+class nova_pochta_lite_init {
+	var $title;
+	var $id;
+	var $email_order_id;
+	function __construct() {
+		$this->id = 'nova_pochta';
+		add_filter('woocommerce_order_shipping_to_display', array($this, 'order_shipping_to_display'), 10 , 2 );
+		add_action( 'woocommerce_shipping_init', 'woocommerce_nova_pochta_init');
+		load_plugin_textdomain( 'wc-saphali-nova-pocta',  false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+		add_action( 'woocommerce_thankyou',                     array( $this, 'order_pickup_location' ), 20 );
+		add_action( 'woocommerce_view_order',                   array( $this, 'order_pickup_location' ), 20 );
+		if( version_compare( WOOCOMMERCE_VERSION, '3.0', '<' ) )
+		add_action( 'woocommerce_after_template_part',          array( $this, 'email_pickup_location' ), 10, 3 );
+		else 
+		add_action( 'woocommerce_after_template_part',          array( $this, 'email_pickup_location' ), 10, 4 );
+		
+		add_action('woocommerce_order_status_pending_to_processing_notification', array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_order_status_pending_to_completed_notification',  array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_order_status_pending_to_on-hold_notification',    array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_order_status_failed_to_processing_notification',  array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_order_status_processing_to_completed_notification',array( $this, 'store_order_id'),1 );
+		add_action('woocommerce_order_status_failed_to_completed_notification',   array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_order_status_completed_notification',             array( $this, 'store_order_id' ),1 );
+		add_action('woocommerce_new_customer_note_notification',                  array( $this, 'store_order_id' ),1 );
+		add_action( 'woocommerce_after_checkout_validation', array( $this, 'after_checkout_validation' ) );
+	}
+	public function after_checkout_validation( $posted ) {
+		if(is_array($posted['shipping_method']) && isset($posted['shipping_method'][0]) )$posted['shipping_method'] = $posted['shipping_method'][0];
+		if ( $posted['shipping_method'] == $this->id ) {
+			if(empty($_POST['nova_pochta'])) {
+				$this->comp_woocomerce_mess_error( __( 'Будь ласка, вкажіть <strong>Адресу або № складу Нової Пошти</strong>.', 'wc-saphali-nova-pocta' ) );
+			}
+		}
+	}
+	function comp_woocomerce_mess_error ($m) {
+		if( version_compare( WOOCOMMERCE_VERSION, '2.1', '<' ) ) {
+			global $woocommerce;
+			$woocommerce->add_error( $m );
+		} else {
+			wc_add_notice( $m, 'error' );
+		}
+	}
+	function order_shipping_to_display($label , $order) {
+		if( empty($this->title) ) {
+			$settings = get_option("woocommerce_{$this->id}_settings", array('title' => '') );
+			$this->title = $settings["title"];
+		}
+		
+		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+			$is_true = (  $order->get_shipping_method() == $this->title || strpos( $this->title, $order->get_shipping_method() ) !== false    ) ;
+		} else {
+			$is_true = ( $order->shipping_method == $this->id ); 
+		}
+
+		if ( $is_true ) {
+			if( strpos($label, 'via') !== false || strpos($label, 'через') !== false )
+			$label = str_replace( array( 'Нова пошта', 'Новая почта', 'Нова Пошта', 'Новая Почта', 'НОВА ПОШТА', 'НОВАЯ ПОЧТА' ), array( 'Нову пошту', 'Новую почту', 'Нову Пошту', 'Новую Почту', 'НОВУ ПОШТУ', 'НОВУЮ ПОЧТУ' ) , $label);
+		}
+		return $label;
+	}
+	public function store_order_id( $arg ) {
+		if ( is_int( $arg ) ) $this->email_order_id = $arg;
+		elseif ( is_array( $arg ) && array_key_exists( 'order_id', $arg ) ) $this->email_order_id = $arg['order_id'];
+	}
+	public function email_pickup_location( $template_name, $template_path, $located, $args = array() ) {
+		if( ( $template_name == 'emails/email-addresses.php' || $template_name == "emails/plain/email-addresses.php") && isset($args["order"]) && is_object($args["order"]) ) {
+			$this->email_order_id  = $args["order"]->get_id();
+		}
+		
+		if( empty($this->title) ) {
+			$settings = get_option("woocommerce_{$this->id}_settings", array('title' => '') );
+			$this->title = $settings["title"];
+		}		
+		if ( $template_name == 'emails/email-addresses.php' && $this->email_order_id ) {
+
+			$order = new WC_Order( $this->email_order_id );
+
+		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+			$is_true = ( $order->get_shipping_method() == $this->title  ) ;
+		} else {
+			$is_true = ($order->shipping_method == $this->id  ); 
+		}
+		if ( $is_true ) {
+			if ( version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+				$pickup_location_array = maybe_unserialize( $order->order_custom_fields['_nova_pochta'][0] );
+			} else {
+				$pickup_location_array =  get_post_meta($order->id, '_nova_pochta', true);			
+			}
+
+			if(!empty($pickup_location_array)) {
+				echo '<div>';
+				echo '<h3>'.__( 'Новая почта', 'wc-saphali-nova-pocta' ).'</h3>';
+				
+				echo '<div>' . __( 'Адрес или № склада Новой Почты', 'wc-saphali-nova-pocta'  )  . ': '.$pickup_location_array .'</div>';
+				echo '</div>';
+			}
+			}
+		} elseif ( $template_name == "emails/plain/email-addresses.php" && $this->email_order_id ) {
+
+			$order = new WC_Order( $this->email_order_id );
+
+		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+			$is_true = ( $order->get_shipping_method() == $this->title  ) ;
+		} else {
+			$is_true = ($order->shipping_method == $this->id  ); 
+		}
+		if ( $is_true ) {
+			if ( version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+				$pickup_location_array = maybe_unserialize( $order->order_custom_fields['_nova_pochta'][0] );
+			} else {
+				$pickup_location_array =  get_post_meta($order->id, '_nova_pochta', true);			
+			}
+
+			if(!empty($pickup_location_array)) {
+				echo "---------------------------------------------------------------------\n";
+				echo __( 'Новая почта', 'wc-saphali-nova-pocta' ) . "\n";
+				
+				echo __( 'Адрес или № склада Новой Почты', 'wc-saphali-nova-pocta'  )  . ': '.$pickup_location_array;
+			}
+			}
+		}
+	}
+	function order_pickup_location($order_id) {
+		if( empty($this->title) ) {
+			$settings = get_option("woocommerce_{$this->id}_settings", array('title' => '') );
+			$this->title = $settings["title"];
+		}
+		$order = new WC_Order( $order_id );
+
+		if ( !version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+			$is_true = ( $order->get_shipping_method() == $this->title  ) ;
+		} else {
+			$is_true = ($order->shipping_method == $this->id  ); 
+		}
+		if ( $is_true ) {
+			if ( version_compare( WOOCOMMERCE_VERSION, '2.1.0', '<' ) ) {
+				$pickup_location_array = maybe_unserialize( $order->order_custom_fields['_nova_pochta'][0] );
+			} else {
+				$pickup_location_array =  get_post_meta($order->id, '_nova_pochta', true);			
+			}
+
+			if(!empty($pickup_location_array)) {
+				echo '<div>';
+				echo '<h3>'.__( 'Новая почта', 'wc-saphali-nova-pocta' ).'</h3>';
+				
+				echo '<div>' . __( 'Адрес или № склада Новой Почты', 'wc-saphali-nova-pocta'  )  . ': '.$pickup_location_array .'</div>';
+				echo '</div>';
+			}
+		}
+	}
+}
+
+add_action('plugins_loaded', 'nova_pochta_lite_init' );
+
+function nova_pochta_lite_init() {
+	new nova_pochta_lite_init();
+}
 
 
 
