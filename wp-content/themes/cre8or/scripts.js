800343b59b30fb021e7cd2b85fc610112dc53cae
antonpash <antonpash@gmail.com>
2017-10-16 13:09:59 +0300

    global updates

--- wp-content/themes/cre8or/scripts.js
+++ wp-content/themes/cre8or/scripts.js
@@ -1,4 +1,4 @@
 jQuery(document).ready(function ($) {
 	$('.yith-wcan-reset-navigation').removeClass('button').addClass('button-sm button-outlined text-black');
-
+	$('.widget_price_filter button[type=submit]').removeClass('button').addClass('button-sm button-outlined text-black');
 });
